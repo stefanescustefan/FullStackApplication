f6039ec7e9eda8afd67ab9505e8f7df1a2f414d2
stefan19 <stefan.stefanescu@stud.ubbcluj.ro>
2023-04-22 13:00:19 +0300

    Added a function to generate unique codes for each room (corrected wrong function name)

--- api/models.py
+++ api/models.py
@@ -7,7 +7,7 @@ def generate_unique_code():
     length = 6
 
     while True:
-        code = ''.join(random.choice(string.ascii_uppercase, k=length))
+        code = ''.join(random.choices(string.ascii_uppercase, k=length))
         if Room.objects.filter(code=code).count() == 0:
             break
 
