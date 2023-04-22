81095bff3849f07dc915f821ece62aa46375bc1b
stefan19 <stefan.stefanescu@stud.ubbcluj.ro>
2023-04-22 12:58:45 +0300

    Added a function to generate unique codes for each room

--- api/models.py
+++ api/models.py
@@ -1,4 +1,18 @@
 from django.db import models
+import string
+import random
+
+
+def generate_unique_code():
+    length = 6
+
+    while True:
+        code = ''.join(random.choice(string.ascii_uppercase, k=length))
+        if Room.objects.filter(code=code).count() == 0:
+            break
+
+    return code
+
 
 # Create your models here.
 class Room(models.Model):
