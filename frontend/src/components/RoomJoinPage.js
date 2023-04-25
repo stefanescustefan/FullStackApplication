2655e7fcb33f53803a957f9b231385c231c2c920
stefan19 <stefan.stefanescu@stud.ubbcluj.ro>
2023-04-25 16:42:04 +0300

    Created the interface for the join page

--- frontend/src/components/RoomJoinPage.js
+++ frontend/src/components/RoomJoinPage.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
+import { TextField, Button, Grid, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 export default class RoomJoinPage extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            roomCode : "",
+            error: ""
+        }
     }
 
     render() {
-        return <p>This is the room join page</p>
+        return (
+            <Grid container spacing={1}>
+                <Grid item xs={12} align="center">
+                    <Typography variant="h4" component="h4">
+                        Join a Room
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <TextField error={this.state.error} label="code" placeholder="Enter a room code" value={this.state.roomCode}
+                               helperText={this.state.error} variant="outlined"/>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Button variant="contained" color="primary">Enter Room</Button>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Button variant="contained" color="secondary" to="/" component={Link}>Back</Button>
+                </Grid>
+            </Grid>
+        );
     }
 }
